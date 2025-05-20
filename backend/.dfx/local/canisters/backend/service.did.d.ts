352e2176f9668b7493a76f1aa68ccc34f1c74813
Hamza1610 <hamza.00dev1@gmail.com>
2025-05-20 22:07:53 +0100

    Implement credential management service in backend, introducing methods for authorizing institutions, issuing credentials, and preparing credential requests. Update service definitions and types in the DID files to reflect new functionality, replacing previous message-related methods.

--- backend/.dfx/local/canisters/backend/service.did.d.ts
+++ backend/.dfx/local/canisters/backend/service.did.d.ts
@@ -3,8 +3,22 @@ import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
 export interface _SERVICE {
-  'getMessage' : ActorMethod<[], string>,
-  'testFunction' : ActorMethod<[], string>,
+  'authorizeInstitution' : ActorMethod<[Principal], string>,
+  'derivation_origin' : ActorMethod<[], string>,
+  'get_credential' : ActorMethod<[string], string>,
+  'issueCredential' : ActorMethod<
+    [
+      {
+        'issueDate' : bigint,
+        'institution' : Principal,
+        'degree' : string,
+        'student' : Principal,
+      },
+    ],
+    bigint
+  >,
+  'prepare_credential' : ActorMethod<[string], string>,
+  'vc_consent_message' : ActorMethod<[], string>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- backend/.dfx/local/canisters/backend/service.did.js
+++ backend/.dfx/local/canisters/backend/service.did.js
@@ -1,7 +1,22 @@
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
-    'getMessage' : IDL.Func([], [IDL.Text], ['query']),
-    'testFunction' : IDL.Func([], [IDL.Text], ['query']),
+    'authorizeInstitution' : IDL.Func([IDL.Principal], [IDL.Text], []),
+    'derivation_origin' : IDL.Func([], [IDL.Text], ['query']),
+    'get_credential' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'issueCredential' : IDL.Func(
+        [
+          IDL.Record({
+            'issueDate' : IDL.Nat64,
+            'institution' : IDL.Principal,
+            'degree' : IDL.Text,
+            'student' : IDL.Principal,
+          }),
+        ],
+        [IDL.Nat64],
+        [],
+      ),
+    'prepare_credential' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'vc_consent_message' : IDL.Func([], [IDL.Text], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
